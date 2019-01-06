d9c7f72d9c1e4ef61da42b73393e4547fd6e9c85
Dantas198 <33515744+Dantas198@users.noreply.github.com>
2019-01-06 18:40:20 +0000

    map

--- src/readWriteMap.java
+++ src/readWriteMap.java
@@ -1,41 +1,45 @@
 import java.util.Map;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
 
-public class readWriteMap<k,v> {
+public class ReadWriteMap<k,v> {
     private final Map<k,v> map;
-    private final ReadWriteLock lock = new ReentrantReadWriteLock();
-    private final Lock r = lock.readLock();
-    private final Lock w = lock.writeLock();
+    private final RWLock lock = new RWLock();
 
-    public readWriteMap(Map<k,v> map){
+    public ReadWriteMap(Map<k,v> map) throws InterruptedException {
         this.map = map;
     }
-    public v put (k key, v value){
-        w.lock();
+    public v putIfAbsent(k key, v value) throws EmailAlreadyUsedException, InterruptedException {
+        lock.writeLock();
+        try{
+            if(map.containsKey(key)) throw new EmailAlreadyUsedException((String)key);
+            return map.put(key,value);
+        }finally {
+            lock.writeUnlock();
+        }
+    }
+    public v put (k key, v value) throws InterruptedException {
+        lock.writeLock();
         try{
             return map.put(key, value);
         }finally {
-            w.unlock();
+            lock.writeUnlock();
         }
     }
-    public boolean remove(k key, v value) {
-        w.lock();
+    public boolean remove(k key, v value) throws InterruptedException {
+        lock.writeLock();
         try{
             return map.remove(key,value);
         } finally {
-            w.unlock();
+            lock.writeUnlock();
         }
     }
     //podem ser adicionados mais métodos
 
-    public v get(Object key){
-        r.lock();
+    public v get(Object key) throws InterruptedException {
+        lock.readLock();
         try{
             return map.get(key);
         } finally {
-            r.unlock();
+            lock.readUnlock();
         }
     }
 }
